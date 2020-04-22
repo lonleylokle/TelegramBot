ed8ae433e681d28ab80cbbbbc789bdbe5de6f57b
Ubuntu <ubuntu@ip-172-31-41-82.eu-west-3.compute.internal>
2020-04-22 12:49:59 +0000

    add unittests

---
+++ tests/test_bot.py
@@ -0,0 +1,39 @@
+import unittest
+
+from bot.Bot import *
+from bot.parserKassir import *
+
+
+class MyTestCase(unittest.TestCase):
+    def
+    def test_send_product(self):
+        self.assertIsNotNone(send_product(123))
+
+    def test_parse_cities(self):
+        self.assertIsNone(parse_cities())
+
+    def test_parser_search(self):
+        self.assertIsNotNone(parser_search("text"))
+
+    def test_search_product(self):
+        test = {"message_id": 2494,
+                "from": {"id": 221468810, "is_bot": "false", "first_name": "Тест", "username": "Test",
+                         "language_code": "en"},
+                "chat": {"id": 221468810, "first_name": "Тест", "username": "Test", "type": "private"},
+                "date": 1587588361,
+                "text": "test"}
+        self.assertIsNotNone(search_product(test))
+
+    def test_help_message(self):
+        test = {"message_id": 2494,
+                "from": {"id": 221468810, "is_bot": "false", "first_name": "Тест", "username": "Test",
+                         "language_code": "en"},
+                "chat": {"id": 221468810, "first_name": "Тест", "username": "Test", "type": "private"},
+                "date": 1587588361,
+                "text": "test"}
+        self.assertIsNone(help_message(test))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
